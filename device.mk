86f1946e68355c7b6f3d11b95a6361f568bdc82e
mekitos <mekitosss@gmail.com>
2016-03-27 11:06:28 +0600

    Update device.mk

--- device.mk
+++ device.mk
@@ -20,5 +20,5 @@ PRODUCT_COPY_FILES += \
 $(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
-PRODUCT_NAME := full_b15q
-PRODUCT_DEVICE := b15q
+PRODUCT_NAME := full_Ixion_P4
+PRODUCT_DEVICE := Ixion_P4
