f87e0ba3599659ff6d90555d0681316e3517554c
mekitos <mekitosss@gmail.com>
2016-03-27 11:26:57 +0600

    Update device.mk

--- device.mk
+++ device.mk
@@ -3,11 +3,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # The gps config appropriate for this device
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
-$(call inherit-product-if-exists, vendor/cat/b15q/b15q-vendor.mk)
+$(call inherit-product-if-exists, vendor/DEXP/Ixion_P4/Ixion_P4-vendor.mk)
 
-DEVICE_PACKAGE_OVERLAYS += device/cat/b15q/overlay
+DEVICE_PACKAGE_OVERLAYS += device/DEXP/Ixion_P4/overlay
 
-LOCAL_PATH := device/cat/b15q
+LOCAL_PATH := device/DEXP/Ixion_P4
 ifeq ($(TARGET_PREBUILT_KERNEL),)
 	LOCAL_KERNEL := $(LOCAL_PATH)/kernel
 else
